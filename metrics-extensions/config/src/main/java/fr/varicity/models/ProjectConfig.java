d4ebc16a99b399efd4b70d79a1015ade933f5f8f
ANAGONOU Patrick <anagonousourou@gmail.com>
2021-11-16 18:04:38 +0100

    start #26

---
+++ metrics-extensions/config/src/main/java/fr/varicity/models/ProjectConfig.java
@@ -0,0 +1,14 @@
+package fr.varicity.models;
+
+import java.util.List;
+
+
+
+public record ProjectConfig(String name, String repositoryUrl, String path, String sourceRoot,SonarCloud sonarcloud, String buildEnv, List<String> buildCmds, String sonarqubeUrl) {
+   
+    public static final record SonarCloud(boolean use, String url){
+
+    }
+}
+
+
