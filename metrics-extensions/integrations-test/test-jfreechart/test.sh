afd49e71fa0ab9f072ca3d9a7c757038fe743156
ANAGONOU Patrick <anagonousourou@gmail.com>
2021-11-20 18:02:53 +0100

    uncomment test

--- metrics-extensions/integrations-test/test-jfreechart/test.sh
+++ metrics-extensions/integrations-test/test-jfreechart/test.sh
@@ -4,7 +4,7 @@ xpdirs=/tmp/varicity-xp-projets
 
 # Run maven tests
 cd ../../../metrics-extension
-mvn clean verify -Dtest=CompilerIT#executeTest -DfailIfNoTests=false
+mvn clean verify -Dtest=CompilerTest#executeTest -DfailIfNoTests=false
 
 if [ ! -d $xpdirs/jfreechart/target ]; then
   echo "No target folder build, did not occured test failed"
