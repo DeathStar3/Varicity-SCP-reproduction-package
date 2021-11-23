7fb0649e174662213e41bfeadabab2f74beb7b8b
Anton van der Tuijn <anton.van-der-tuijn@etu.unice.fr>
2021-11-23 18:50:46 +0100

    #41 Change default working directory in config file, and update fields names in config file

--- metrics-extension/entities/src/main/java/fr/unice/i3s/sparks/deathstar3/model/MetricSource.java
+++ metrics-extension/entities/src/main/java/fr/unice/i3s/sparks/deathstar3/model/MetricSource.java
@@ -1,5 +1,6 @@
 package fr.unice.i3s.sparks.deathstar3.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,16 +14,24 @@ import java.util.List;
 @AllArgsConstructor
 public class MetricSource {
 
+    @JsonProperty("name")
     private String name; // Mandatory
+    @JsonProperty("enabled")
     private boolean enabled = true; // Optional (default to true)
 
+    @JsonProperty("shell-location")
     private String shellLocation; // Optional (default powershell or sh)
+    @JsonProperty("working-directory")
     private String workingDirectory = System.getProperty("user.home"); // Optional (default user home directory)
+    @JsonProperty("commands")
     private List<String> commands; // Optional
 
+    @JsonProperty("component-name")
     private String componentName; // Mandatory
+    @JsonProperty("root-url")
     private String rootUrl; // Mandatory
 
+    @JsonProperty("metrics")
     private List<String> metrics;
 
     @Override
