52c699b4ad8bb73ca5011ee6ae49a89dd009836c
Anton-vanderTuijn <anton.van-der-tuijn@etu.unice.fr>
2021-11-24 20:15:02 +0100

    #41 Add support for linux : integration test metrics-extension

--- metrics-extension/integration-tests/test_integration_sonarqube_windows.sh
+++ metrics-extension/integration-tests/test_integration_sonarqube_windows.sh
@@ -8,10 +8,11 @@ echo " ### ### [1;32m Integration test for metric-extension (windows) [0m### #
 
 echo " ### [1;32m Step: 0/3 - Clean environement [0m"
 rm -R -d ./test_actual_result
-#rm -R -d ./target
+rm -R -d ./../target
 
 echo " ### [1;32m Step: 1/3 - Build metrics-extension [0m"
 cd ./../
+chmod +x ./build.sh
 ./build.sh
 cd ./integration-tests
 
@@ -24,9 +25,9 @@ RESULT_FILE_NAME=$(find ./test_actual_result -type f -printf "%f\n") # Find file
 
 DIFF=$(diff ./test_expected_result/junit-r4.13.2-expected.json ./test_actual_result/junit-r4.13.2/${RESULT_FILE_NAME}) #Compare files
 
-if [ -z "${DIFF}" ]; then # Check if the comparison result is an empty string
-  echo " ### [1;32m TEST SUCCESS [0m"
-  exit 0 # Success
-fi
+if [ ! -z "${DIFF}" ]; then # Check if the comparison result is an empty string
   echo " ### [1;31m TEST FAIL [0m"
   exit 1 # Fail
+fi
+  echo " ### [1;32m TEST SUCCESS [0m"
+  exit 0 # Success
