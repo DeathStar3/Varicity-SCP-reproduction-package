832fd36c26d5dd25fc3b09b8d17fbb2ca947f7fe
Anton van der Tuijn <anton.van-der-tuijn@etu.unice.fr>
2021-11-24 16:41:23 +0100

    #41 Start implementing integration test for metrics-extension - Add sources of Junit-r4.13.2 + custom pom - Implemented integration test for windows (linux not tested) - Remove integration-test maven module

---
+++ metrics-extension/integration-tests/test_integration_sonarqube_linux.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+  #########################################################
+  # Metrics-Extension Integration test (linux)
+  #########################################################
+
+echo " ### ### [1;32m Integration test for metric-extension (linux) [0m### ### "
+
+echo " ### [1;32m Step: 0/3 - Clean environement [0m"
+rm -R -d ./test_actual_result
+#rm -R -d ./target
+
+echo " ### [1;32m Step: 1/3 - Build metrics-extension [0m"
+cd ./../
+./build.sh
+cd ./integration-tests
+
+echo " ### [1;32m Step: 2/3 - Run metrics-extension [0m"
+java -jar ./../target/metrics-extension-1.0-SNAPSHOT.jar ./test_config_file/junit-r4.13.2-config-linux.yaml debug
+
+echo " ### [1;32m Step: 3/3 - Assert result [0m"
+
+RESULT_FILE_NAME=$(find ./test_actual_result -type f -printf "%f\n") # Find file name
+
+DIFF=$(diff ./test_expected_result/junit-r4.13.2-expected.json ./test_actual_result/junit-r4.13.2/${RESULT_FILE_NAME}) #Compare files
+
+if [ -z "${DIFF}" ]; then # Check if the comparison result is an empty string
+  echo " ### [1;32m TEST SUCCESS [0m"
+  exit 0 # Success
+fi
+  echo " ### [1;31m TEST FAIL [0m"
+  exit 1 # Fail
