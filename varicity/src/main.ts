eec7494e22063aefba88c23d0eb89c0cb6cd632a
ANAGONOU Patrick <anagonousourou@gmail.com>
2021-11-25 22:16:08 +0100

    add name for config

--- varicity/src/main.ts
+++ varicity/src/main.ts
@@ -5,7 +5,11 @@ import { ConfigLoader } from './controller/parser/configLoader';
 import { UIController } from './controller/ui/ui.controller';
 import { Vector3_Local } from './model/entitiesImplems/config.model';
 import { ProjectService } from './services/project.service';
-
+import '@material/mwc-dialog';
+import '@material/mwc-button/mwc-button';
+import '@material/mwc-radio/mwc-radio';
+import '@material/mwc-formfield/mwc-formfield';
+import { Closeable } from './model/entities/closeable.interface';
 class Main {
 
 
@@ -21,8 +25,10 @@ class Main {
             UIController.createConfig(UIController.config);
         });
 
-        document.querySelector("#save-config").addEventListener('click', _clickev => {
-            console.log('Add config ', new Date().toISOString())
+        document.querySelector('#save-config').addEventListener('click', _clickev => {
+
+           
+            document.querySelector('#dialog').setAttribute('open','true');
 
             console.log('Project Id of the Config is', UIController.config.projectId);
 
@@ -30,14 +36,30 @@ class Main {
             UIController.config.projectId = Cookies.get('varicity-current-project');
             console.log('Project Id of the Config is now', UIController.config.projectId);
 
+            (document.querySelector('#text-field') as HTMLInputElement).value= UIController.config.name;
+            
+        });
+
+        document.querySelector('#save-config-confirm-btn').addEventListener('click', _clickev=>{
+            console.log('Add config ', new Date().toISOString())
+
+            
+            UIController.config.name=(document.querySelector('#text-field') as HTMLInputElement).value;
+
+            //récuperer le texte entré comme nom de la config
+            //fermer le dialog
+
             axios.post(`${backendUrl}/projects/configs`, UIController.config).then(response => {
+                console.log('Config saved successfully');
                 UIController.config = response.data;
 
             }).catch(err => {
                 console.log('Cannot save config to database');
                 console.error(err);
-            })
-        });
+            });
+
+           ( document.querySelector('#dialog')as unknown as Closeable).close();
+        })
 
     }
 
--- varicity/webpack.config.js
+++ varicity/webpack.config.js
@@ -27,10 +27,10 @@ module.exports = {
     devServer: {
         host: '0.0.0.0',
         port: 9090, //port that we're using for local host (localhost:9090)
-        disableHostCheck: true,
-        contentBase: path.resolve(appDirectory, "public"), //tells webpack to serve from the public folder
-        publicPath: '/',
         hot: true
+        /* object { allowedHosts?, bonjour?, client?, compress?, devMiddleware?, headers?, historyApiFallback?, host?, hot?,
+           http2?, https?, ipc?, liveReload?, magicHtml?, onAfterSetupMiddleware?, 
+          onBeforeSetupMiddleware?, onListening?, open?, port?, proxy?, server?, setupExitSignals?, static?, watchFiles?, webSocketServer? }*/
     },
     devtool: 'eval-source-map',
     module: {
