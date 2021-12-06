7f2786fdf0383b46a8bc2b8fa1b31f699f3afec2
Anton van der Tuijn <anton.van-der-tuijn@etu.unice.fr>
2021-12-06 16:40:08 +0100

    #68 Start implementing building color (without drag and drop)

--- varicity/src/controller/ui/menu/building.controller.ts
+++ varicity/src/controller/ui/menu/building.controller.ts
@@ -1,6 +1,7 @@
 import {SubMenuController} from "./sub-menu.controller";
 import {UIController} from "../ui.controller";
 import {CriticalLevel} from "../../../model/entitiesImplems/config.model";
+import {Color} from "../../../model/entities/config.interface";
 
 export class BuildingController {
 
@@ -13,15 +14,15 @@ export class BuildingController {
         const menuPadding = SubMenuController.createMenu("Padding", true, parent);
         const menuFacesColors = SubMenuController.createMenu("Faces Color", true, parent);
         const menuEdgesColors = SubMenuController.createMenu("Edges Color", true, parent);
-        const menuOutlinesColors = SubMenuController.createMenu("Outliles Color", true, parent);
+        const menuOutlinesColors = SubMenuController.createMenu("Outlines Color", true, parent);
 
         if (UIController.config) {
 
             // Fetch the attributes
             let padding = UIController.config.building.padding;
             let colorsFaces = UIController.config.building.colors.faces;
-            let colorsEdges = UIController.config.building.colors;
-            let colorsOutlines = UIController.config.building.colors;
+            let colorsEdges = UIController.config.building.colors.edges;
+            let colorsOutlines = UIController.config.building.colors.outlines;
 
             // Padding
             let paddingSelector = SubMenuController.createRange("Padding", padding, 0.2, 2, 0.1, menuPadding);
@@ -33,7 +34,44 @@ export class BuildingController {
             })
 
             // Face Colors
-            //TODO need to implement Face, Edge and Outlines colors
+
+            const listOfProperties = ["API", "CLASS", "INTERFACE", "ABSTRACT", "VP", "VARIANT", "HOTSPOT", "STRATEGY", "FACTORY", "TEMPLATE", "DECORATOR", "COMPOSITION_STRATEGY", "METHOD_LEVEL_VP", "DENSE", "PUBLIC", "PRIVATE"]
+
+            if (colorsFaces) {
+                colorsFaces.forEach(color => {
+                    if (!color.color){ color.color = "#555555"}
+                    let colorPicker = SubMenuController.createColorSelector(color.name, color.color, menuFacesColors);
+
+                    colorPicker.addEventListener("change", (ke) => {
+                        color.color = colorPicker.value
+                        UIController.updateScene(CriticalLevel.LOW_IMPACT); // Reload the screen
+                    });
+                })
+            }
+
+            if (colorsEdges) {
+                colorsEdges.forEach(color => {
+                    if (!color.color){ color.color = "#555555"}
+                    let colorPicker = SubMenuController.createColorSelector(color.name, color.color, menuEdgesColors);
+
+                    colorPicker.addEventListener("change", (ke) => {
+                        color.color = colorPicker.value
+                        UIController.updateScene(CriticalLevel.LOW_IMPACT); // Reload the screen
+                    });
+                })
+            }
+
+            if (colorsOutlines) {
+                colorsOutlines.forEach(color => {
+                    if (!color.color){ color.color = "#555555"}
+                    let colorPicker = SubMenuController.createColorSelector(color.name, color.color, menuOutlinesColors);
+
+                    colorPicker.addEventListener("change", (ke) => {
+                        color.color = colorPicker.value
+                        UIController.updateScene(CriticalLevel.LOW_IMPACT); // Reload the screen
+                    });
+                })
+            }
         }
     }
 }
