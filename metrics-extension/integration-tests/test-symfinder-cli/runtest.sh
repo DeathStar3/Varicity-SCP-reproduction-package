fe9b1b41b2bfac5fba123ea87da2820a761909ad
ANAGONOU Patrick <anagonousourou@gmail.com>
2021-12-06 23:24:52 +0100

    updating tests

--- metrics-extension/integration-tests/test-symfinder-cli/runtest.sh
+++ metrics-extension/integration-tests/test-symfinder-cli/runtest.sh
@@ -1,9 +1,17 @@
-
+#!/bin/bash
 set -e
 
+#before
+sudo apt-get install wait-for-it
+python -m pip install requests
+docker network ls | grep varicity-config > /dev/null || docker network create --driver bridge varicity-config
+docker run --expose 3000 --name varicity-backend --network varicity-config --detach -p 3000:3000 deathstar3/varicity-backend:local
+
+wait-for-it -t 0 localhost:3000
+
 #test itself
-bash test_integration_sonarqube_linux.sh
+docker run -v $(pwd)/test_config_file:/data --network varicity-config -v /var/run/docker.sock:/var/run/docker.sock:ro -e RUNTIME_MODE=DOCKER  deathstar3/symfinder-cli:local -i /data/junit-r4.13.2-config.yaml -s /data/symfinder.yaml -verbosity INFO -http http://varicity-backend:3000/projects
 
-
+python main.py
 #after
 docker container rm --force $(docker container ls -a --quiet)
