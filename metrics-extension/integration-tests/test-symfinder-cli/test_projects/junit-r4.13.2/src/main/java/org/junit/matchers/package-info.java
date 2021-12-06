8d00b77ee3c002f7d9fc47f2c68136d29395324a
ANAGONOU Patrick <anagonousourou@gmail.com>
2021-12-06 17:35:18 +0100

    remove junit

--- metrics-extension/integration-tests/test-symfinder-cli/test_projects/junit-r4.13.2/src/main/java/org/junit/matchers/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Provides useful additional {@link org.hamcrest.Matcher}s for use with
- * the {@link org.junit.Assert#assertThat(Object, org.hamcrest.Matcher)}
- * statement
- *
- * @see org.junit.matchers.JUnitMatchers
- * @since 4.0
- */
-package org.junit.matchers;
--- metrics-extension/integration-tests/test-symfinder-cli/test_projects/junit-r4.13.2/src/test/java/org/junit/tests/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Tests the JUnit functionality.
- * <p>
- * Corresponds to {@link junit.tests.AllTests} in Junit 3.x.
- *
- * @since 4.0
- */
-package org.junit.tests;
