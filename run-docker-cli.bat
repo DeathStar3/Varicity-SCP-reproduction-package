f63d6bed0020b0bf30f3d2835c8927026928dc69
Patrick S <anagonousourou@gmail.com>
2021-12-08 17:00:44 +0100

    wrote bat equivalent of sh

---
+++ run-docker-cli.bat
@@ -0,0 +1,4 @@
+
+
+docker network ls | findstr varicity-config > NUL || docker network create --driver bridge varicity-config
+docker run -v "%cd%"\data:/data --network varicity-config -v //var/run/docker.sock:/var/run/docker.sock:ro -e RUNTIME_MODE=DOCKER  deathstar3/symfinder-cli:local %*
