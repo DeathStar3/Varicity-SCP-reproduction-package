90dba66115651cb675faec3f73ea57a977871aea
Anton van der Tuijn <anton.van-der-tuijn@etu.unice.fr>
2021-12-10 17:29:43 +0100

    Remove unused code and files

--- metrics-extension/compiler-integration-test-OLD/run-custom-sonarqube.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker network ls | grep varicity-config > /dev/null || docker network create --driver bridge varicity-config
-
-docker run --name sonarqubehost -p 9000:9000 --network varicity-config --expose=9000 varicity-sonarqube 
