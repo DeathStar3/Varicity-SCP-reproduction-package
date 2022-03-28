0bc69bb6fa2c22ab6e374b4793a2ef4df45d0aa7
Johann Mortara <johann.mortara@univ-cotedazur.fr>
2022-03-28 14:01:23 +0200

    Sending / receiving large JSON for big projects (up to 500MB)

--- varicity-backend/src/main.ts
+++ varicity-backend/src/main.ts
@@ -18,8 +18,8 @@ async function startServer() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.use(bodyParser.json({ limit: '50mb' }));
-  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  app.use(bodyParser.json({ limit: '500mb' }));
+  app.use(bodyParser.urlencoded({ limit: '500mb', extended: true }));
   app.enableCors();
 
   await app.listen(process.env.PORT || 3000);
