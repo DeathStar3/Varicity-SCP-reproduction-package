0ca7d12a124474e4c46751bf650ff50e9a65c370
Yann-Braultr <brlt.yann@gmail.com>
2022-05-12 08:32:05 +0200

    test cases
    #37 #38

--- js/run_test.sh
+++ js/run_test.sh
@@ -1,16 +1,18 @@
 project_tested="$1"
+project_path=test_project/$project_tested
+test_file=$project_tested.test.ts 
 
 
-echo "Running test for: $project_tested"
+echo "Running test for: $project_path"
 
 cd app
 npm run --silent build
 
-PROJECT_PATH=$project_tested node lib/index.js
+PROJECT_PATH=$project_path node lib/index.js
 
 echo "Go check the result on port 7474"
 echo "Corresponding database has been downloaded into db.json"
 
 echo "running test ..."
 
-npm t 
+npm test -- $test_file   
