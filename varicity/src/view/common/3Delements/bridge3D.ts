c0d7e5c88ae61a9bf93cec1425fdc55ed9abbe98
GabrielCogne <gabriel.cogne@gmail.com>
2023-02-14 22:43:54 +0100

    Add bridge link between file and class #12

---
+++ varicity/src/view/common/3Delements/bridge3D.ts
@@ -0,0 +1,78 @@
+import { Link3D } from '../3Dinterfaces/link3D.interface';
+import { Color3, Color4, Curve3, Mesh, MeshBuilder, Scene, StandardMaterial, Vector3 } from "@babylonjs/core";
+import { Building3D } from "./building3D";
+import { Config } from "../../../model/entitiesImplems/config.model";
+
+export class Bridge3D implements Link3D {
+	scene: Scene;
+
+	src: Building3D;
+	dest: Building3D;
+	type: string;
+
+	force: boolean = false;
+
+	config: Config;
+
+	private mesh: Mesh;
+	private material: StandardMaterial;
+	private center: Vector3;
+
+	constructor(src: Building3D, dest: Building3D, type: string, scene: Scene, config: Config) {
+		this.src = src;
+		this.dest = dest;
+		this.type = type;
+		this.scene = scene;
+		this.config = config;
+	}
+
+	display(force?: boolean, show?: boolean) {
+		if (force != undefined) this.force = force;
+		if (!show && !this.force && this.mesh) {
+			this.render(false);
+		} else {
+			if (show && ((force == undefined || this.force) && !this.mesh))
+				this.render(true);
+		}
+	}
+
+	private getName(): string {
+		return this.src.elementModel.name + " => " + this.dest.elementModel.name
+	}
+
+	private createBridge(src: Vector3, dest: Vector3) {
+		console.log("Link ", this.getName(), " between ", src, " and ", dest);
+		this.center = Vector3.Center(src, dest);
+
+		let curve = Curve3.CreateQuadraticBezier(
+			src,
+			Vector3.Center(src, dest).add(new Vector3(0, Math.max(src.y, dest.y), 0)),
+			dest,
+			25
+		);
+
+		console.log("Points drawn: ", curve.getPoints());
+
+		let colors = []
+		for (const _ of curve.getPoints())
+			colors.push(Color4.FromHexString("#335DFFFF"))
+
+		this.mesh = MeshBuilder.CreateLines(
+			this.getName(),
+			{
+				points: curve.getPoints(),
+				colors: colors
+			},
+			this.scene
+		)
+	}
+
+	render(display: boolean): void {
+		if (display) {
+			this.createBridge(this.src.center, this.dest.center);
+		} else {
+			this.mesh?.dispose();
+			delete this.mesh;
+		}
+	}
+}
--- varicity/src/view/common/3Delements/link3DImplem.ts
+++ varicity/src/view/common/3Delements/link3DImplem.ts
@@ -56,58 +56,13 @@ export class Link3DImplem implements Link3D {
         this.mesh = MeshBuilder.CreateLines("curve", {points: this.curve.getPoints(), colors: colors}, this.scene);
     }
 
-    // render(config: Config): void {
-    //     this.curve = Curve3.CreateQuadraticBezier(this.src.top, this.src.top.add(new Vector3(0, (this.src.top.y + this.dest.top.y) / 2, 0)), this.dest.top, 25);
-
-    //     let colors: Color4[] = [];
-    //     let start = Color4.FromColor3(Color3.Red()); // default value
-    //     let end = Color4.FromColor3(Color3.Black()); // darkens with distance
-
-    //     if (config.link.colors) {
-    //         for (let c of config.link.colors) {
-    //             let done = false;
-    //             if (c.name == this.type) {
-    //                 start = Color4.FromColor3(Color3.FromHexString(c.color));
-    //                 done = true;
-    //             }
-    //             if(done) break;
-    //         }
-    //     }
-
-    //     for (let i = 0; i < this.curve.getPoints().length; i++) {
-    //             colors.push(Color4.Lerp(start, end, i/this.curve.getPoints().length));
-    //     }
-
-    //     this.mesh = MeshBuilder.CreateLines("curve", { points: this.curve.getPoints(), colors: colors }, this.scene);
-    //     this.mesh.visibility = 0; // defaults at hidden
-
-    //     // this.line = MeshBuilder.CreateLines("curve", {points: this.curve.getPoints()}, this.scene);
-    //     // this.line.visibility = 0; // defaults at hidden
-    //     // if(config.link.colors) {
-    //     //     for(let c of config.link.colors) {
-    //     //         if(c.name == this.type) {
-    //     //             this.line.color = Color3.FromHexString(c.color);
-    //     //             return;
-    //     //         }
-    //     //     }
-    //     // }
-    // }
-
-    // hide(force?: boolean) {
-    //     if(force) this.force = false;
-    //     if(!this.force) this.line.visibility = 0;
-    // }
-
     display(force?: boolean, show?: boolean) {
         if (force != undefined) this.force = force;
-        // if (!this.force && this.mesh.visibility == 1) {
         if (!show && !this.force && this.mesh) {
-            // this.mesh.visibility = 0;
             this.render(false);
         } else {
             if (show && ((force == undefined || this.force) && !this.mesh))
-                this.render(true); // this.mesh.visibility = 1;
+                this.render(true);
         }
-        // this.line.visibility = 1;
     }
 }
