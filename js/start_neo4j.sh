2ca477a7280968aca560f80e51266451d2a378fb
Philippe Collet <Philippe.Collet@unice.fr>
2023-02-15 17:53:13 +0100

    fix: switch to full path for docker volumes in neo4j starter script

--- js/start_neo4j.sh
+++ js/start_neo4j.sh
@@ -2,8 +2,8 @@ if ! docker  ps -a | grep -q symfinderts_neo4j; then
   echo "Starting Docker"
   docker run --name symfinderts_neo4j -d -p7474:7474 -p7687:7687 -e NEO4J_AUTH=neo4j/root \
   -e NEO4JLABS_PLUGINS='["apoc"]' \
-  --volume ./conf:/conf \
-  --volume ./import:/import \
+  --volume $(pwd)/conf:/conf \
+  --volume $(pwd)/import:/import \
   neo4j:4.1.9
   echo "Import base"
   ./import_base.sh > /dev/null
